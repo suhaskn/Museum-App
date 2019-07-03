c30ca1769097db79517240b2b52e3ed02815b505
Suhas <suhaskn09@gmail.com>
2019-07-03 23:58:21 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,17 +1,23 @@
 function submitComment(){
-    const inputField = document.getElementById('name')
+    const inputField = document.getElementById('name');
     const name = inputField.value;
-    const textArea = document.getElementById('msg')
-    const msg = textArea.value
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
     console.log(name);
     console.log(msg);
-    const comment = document.createElement('section')
-    const h3 = document.createElement('h3')
-    const p = document.createElement('p')
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
     h3.innerHTML = `${name} said:`;
     p.innerHTML = msg;
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
+
     console.log(comment);
+    const commentSection = document.getElementById('comments');
+    console.log(commentSection);
+    commentSection.appendChild(comment);
+    inputField.value = null;
+    textArea.value = null;
 }
